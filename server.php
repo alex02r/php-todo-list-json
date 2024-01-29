7e89b7f8d085dc8820799640bd4fc8da5f0fbe84
alex02r <alex.giochi02@gmail.com>
2024-01-29 16:04:58 +0100

    [UPGRADE] funzione che cambia il valore del task

--- server.php
+++ server.php
@@ -3,5 +3,13 @@
 
     header('Content-Type: application/json');
 
+    if (isset($_POST['index_item'])) {
+        $data = json_decode($list, true);
+        $data[$_POST['index_item']]['do'] = !$data[$_POST['index_item']]['do'];
+
+        $newList = json_encode($data, true);
+        file_put_contents('./todo.json', $newList);
+    }
+
     echo $list;
 ?>
